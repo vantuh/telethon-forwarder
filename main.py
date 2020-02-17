213341da9a05a42041c96f381699da52dfb41e90
Ivan <i.mykytiuk@temabit.com>
2020-02-17 11:39:18 +0200

    [chg] listener corrections

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ class Forwarder:
             for (dirpath, dirnames, filenames) in os.walk(os.environ['CHANNELS_DIR']):
                 for filename in filenames:
                     with FileWorker("%s/%s" % (dirpath, filename)) as file_worker:
-                        for channel in file_worker.channels:
-                            ChannelListner(client, ForwardOption(channel, Channel('%s' % (os.path.splitext(filename)[0]), '', 1)))
+                        for from_channel in file_worker.channels:
+                            id_str = '%s' % (os.path.splitext(filename)[0])
+                            to_channel = Channel(id_str, id_str, 1)
+                            ChannelListner(client, ForwardOption(from_channel, to_channel))
 
             client.run_until_disconnected()
 
