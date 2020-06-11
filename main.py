ca88b38df0a7410cf9e55bc0f1fea90f4ca7cebf
Ivan <i.mykytiuk@temabit.com>
2020-06-11 13:48:37 +0300

    [chg] csv check

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from utils import ChannelListner, FileWorker
 
 class Forwarder:
     def __init__(self):
-        with TelegramClient(os.environ['SESSION_NAME'], os.environ['API_ID'], os.environ['API_HASH']) as client:
-            for (dirpath, dirnames, filenames) in os.walk(os.environ['CHANNELS_DIR']):
+        with TelegramClient(SESSION_NAME, API_ID, API_HASH) as client:
+            for dirpath, dirnames, filenames in os.walk("channels"):
                 for filename in filenames:
-                    with FileWorker("%s/%s" % (dirpath, filename)) as file_worker:
-                        if len(file_worker.channels) > 1:
-                            to_channel = Channel(file_worker.channels[0].identifier, file_worker.channels[0].name, 1)
-                            ChannelListner(client, ForwardOption(file_worker.channels, to_channel))
+                    if filename.endswith(".csv"):
+                        with FileWorker("%s/%s" % (dirpath, filename)) as file_worker:
+                            if len(file_worker.channels) > 1:
+                                to_channel = Channel(
+                                    file_worker.channels[0].identifier, file_worker.channels[0].name, 1)
+                                ChannelListner(client, ForwardOption(file_worker.channels, to_channel))
 
             client.run_until_disconnected()
 
