50f72aaca41b39d3ba2974d3556ef88dbff8f479
Ivan <i.mykytiuk@temabit.com>
2020-06-12 02:54:18 +0300

    cut pandas for night instance upload

--- utils/FileWorker.py
+++ utils/FileWorker.py
@@ -1,6 +1,6 @@
-import pandas
+import csv
 import datetime
-# from classes import Channel
+from classes import Channel
 
 
 class FileWorker:
@@ -10,23 +10,11 @@ class FileWorker:
 
     def __fetch_channels__(self):
         self.channels = []
-        file = pandas.read_csv(self.filename)
-        for line in file:
-            print(line)
-        # self.channels.append()
-        # file = open(self.file_name, mode='r')
-        # reader = csv.DictReader(file, delimiter=',')
-        # for line in reader:
-        #     self.channels.append(
-        #         Channel(line["identifier"], line["name"], datetime.datetime.today()))
-
-    # def update_channel(self):
-        # file = open(self.file_name, mode='w')
-        # writer = csv.DictWriter(
-        #     file, fieldnames=['name', 'identifier', 'date'])
-
-        # writer.writeheader()
-        # writer.writerows()
+        file = open(self.filename, mode='r')
+        reader = csv.DictReader(file, delimiter=',')
+        for line in reader:
+            self.channels.append(
+                Channel(line["identifier"], line["name"], datetime.datetime.today()))
 
     def __enter__(self):
         return self
