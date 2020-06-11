d821c2dc927cd2c481597c7c36810273c27baee1
i.mykytiuk <i.mykytiuk@temabit.com>
2020-06-11 15:54:09 +0300

    dotenv implementation

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ADD . /app
 WORKDIR /app
 
 RUN pip3 install pytz
+RUN pip3 install python-dotenv
 RUN pip3 install telethon
 
 CMD [ "python3", "./main.py" ]
--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from utils import ChannelListner, FileWorker
 
 class Forwarder:
     def __init__(self):
-        with TelegramClient(SESSION_NAME, API_ID, API_HASH) as client:
-            for dirpath, dirnames, filenames in os.walk("channels"):
+        with TelegramClient(os.getenv("SESSION_NAME"), os.getenv("API_ID"), os.getenv("API_HASH")) as client:
+            for dirpath, _, filenames in os.walk(os.getenv("CHANNELS_DIR")):
                 for filename in filenames:
                     if filename.endswith(".csv"):
                         with FileWorker("%s/%s" % (dirpath, filename)) as file_worker:
                             if len(file_worker.channels) > 1:
                                 to_channel = Channel(
                                     file_worker.channels[0].identifier, file_worker.channels[0].name, 1)
-                                ChannelListner(client, ForwardOption(file_worker.channels, to_channel))
+                                ChannelListner(client, ForwardOption(
+                                    file_worker.channels, to_channel))
 
             client.run_until_disconnected()
 
